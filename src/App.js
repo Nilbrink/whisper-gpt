e5a1b2f46903a0ec30727da4a264faf17a41cb5e
Nilbrink <fredrik.nilbrink@ri.se>
2023-05-05 15:55:39 +0200

    Adjustments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useWhisper } from '@chengsokdara/use-whisper'
 
-function App() {
+const App = () => {
+  const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: '[din apinyckel]', // YOUR_OPEN_AI_TOKEN
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Recording: {recording}</p>
+      <p>Speaking: {speaking}</p>
+      <p>Transcribing: {transcribing}</p>
+      <p>Transcribed Text: {transcript.text}</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => pauseRecording()}>Pause</button>
+      <button onClick={() => stopRecording()}>Stop</button>
     </div>
-  );
+  )
 }
 
 export default App;
